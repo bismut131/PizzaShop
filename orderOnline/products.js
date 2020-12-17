8f00366ae053e2716856e98bda9ef68a7df7a379
muko <m_vadachkoria@cu.edu.ge>
2020-12-17 19:09:33 +0400

    warnings

---
+++ orderOnline/products.js
@@ -0,0 +1,19 @@
+
+
+// document.getElementsByClassName('.order-online-products').innerHTML = 'muko';
+
+// var xhr = new XMLHttpRequest;
+
+// xhr.onload = function () {
+//     responseObject = JSON.parse(xhr.responseText);
+// }
+
+// var newContent = "";
+// for(var i = 0; i < responseObject.products.length; i++){
+//     newContent += '<article class="product">'
+//     newContent += '<img src="' + responseObject.products[i].img +'" />';
+//     newContent +=  '<p>' + responseObject.products[i].description + '</p>'
+//     newContent += '</article>'
+// }
+// xhr.open('GET', 'products.json', true);
+// xhr.send(null);
