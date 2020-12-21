a15b09565e106e9b6a889257cf4b77760d86d918
muko <m_vadachkoria@cu.edu.ge>
2020-12-21 20:48:55 +0400

    designing shopping-cart

--- shared.js
+++ shared.js
@@ -1,21 +1,25 @@
-    var $button = $(".toggle-button");
-    var $mobileNav = $(".mobile-nav");
+var $button = $(".toggle-button");
+var $mobileNav = $(".mobile-nav");
+var $shoppingCard = $(".shopping-card");
+var $shoppingCardIcon = $(".shopping-card-icon");
 
-    $button.click(function () {
-        if($mobileNav.hasClass("open"))
-            $mobileNav.removeClass("open");
-        else $mobileNav.addClass("open");
-    })
+$button.click(function () {
+    if($mobileNav.hasClass("open"))
+        $mobileNav.removeClass("open");
+    else {
+        $mobileNav.addClass("open");
+        $shoppingCard.removeClass("openflex");
+    }
+})
 
-    var $shoppingCartIcon = $('.shopping-cart-icon-img');
-    var $exitIcon = $('.exit-icon-img');
-    var $shoppingCart = $('.shopping-cart');
+$shoppingCardIcon.click(function () {
+    if($shoppingCard.hasClass("openflex"))
+        $shoppingCard.removeClass("openflex");
+    else {
+        $shoppingCard.addClass("openflex");
+        $mobileNav.removeClass("open");
+    }
+})
 
-    $shoppingCartIcon.click(function() {
-        $shoppingCart.addClass("open");
-    })
-
-    $exitIcon.click(function () {
-        $shoppingCart.removeClass("open");
-    })
+    
 
